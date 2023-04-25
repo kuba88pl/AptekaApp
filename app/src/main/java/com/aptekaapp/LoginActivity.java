6fd8bf420597b27019830385fcba5a54fa155704
Jakub Kulawik <kuba@MacBook-Air-Jakub.local>
2023-04-25 22:46:41 +0200

    Firebase connected, register and login dont work

--- app/src/main/java/com/aptekaapp/LoginActivity.java
+++ app/src/main/java/com/aptekaapp/LoginActivity.java
@@ -2,9 +2,17 @@ package com.aptekaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email, password;
+    ImageButton buttonLogin;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/aptekaapp/RegisterActivity.java
+++ app/src/main/java/com/aptekaapp/RegisterActivity.java
@@ -2,13 +2,38 @@ package com.aptekaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class RegisterActivity extends AppCompatActivity {
-
+ImageButton loginBtn, registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        loginBtn = findViewById(R.id.signin);
+        registerBtn = findViewById(R.id.signup);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
     }
+
+
 }
