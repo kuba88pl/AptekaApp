76129168ad9d2cad6ba5c8e3cdfd9f999eedf6ce
Jakub Kulawik <kuba@MacBook-Air-Jakub.local>
2023-04-28 09:09:50 +0200

    User class added

--- app/src/main/java/com/aptekaapp/model/Adress.java
+++ app/src/main/java/com/aptekaapp/model/Adress.java
@@ -1,2 +1,9 @@
-package com.aptekaapp.model;public class Adress {
+package com.aptekaapp.model;
+
+public class Adress {
+    String city;
+    String street;
+    int houseNumber;
+    int homeNumber;
+
 }
