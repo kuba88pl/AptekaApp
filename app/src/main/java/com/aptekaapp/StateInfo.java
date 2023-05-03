19cb6e42788fe25b3fe01aea6fe404423a39c3f2
kuba88pl <jakub.kulawik@gmail.com>
2023-05-03 21:46:58 +0200

    Firebase authorisation done - register and login works fine. Login status check doesn't run on MainActivity

---
+++ app/src/main/java/com/aptekaapp/StateInfo.java
@@ -0,0 +1,14 @@
+package com.aptekaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StateInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_state_info);
+    }
+}
