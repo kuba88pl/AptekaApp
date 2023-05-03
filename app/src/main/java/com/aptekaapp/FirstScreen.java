260f07fcf82072c611a7437898831395175f717f
kuba88pl <jakub.kulawik@gmail.com>
2023-05-03 11:08:24 +0200

    Deleted some activities, replaced new with linear layout

--- app/src/main/java/com/aptekaapp/FirstScreen.java
+++ app/src/main/java/com/aptekaapp/FirstScreen.java
@@ -2,21 +2,22 @@ package com.aptekaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
-    EditText email, password;
-    ImageButton buttonLogin;
-    FirebaseAuth mAuth;
+public class FirstScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+
+
+        }
+
 }
