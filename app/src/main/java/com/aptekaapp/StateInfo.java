f51e0a8b67905cf601d4fa225db6288a4eca308a
Goldmabachmajorian <sebastian13333@gmail.com>
2023-05-05 22:56:49 +0200

    Created state Activity

--- app/src/main/java/com/aptekaapp/StateInfo.java
+++ app/src/main/java/com/aptekaapp/StateInfo.java
@@ -2,13 +2,30 @@ package com.aptekaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class StateInfo extends AppCompatActivity {
+
+
+
+    ImageButton backBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state_info);
+        backBtn=findViewById(R.id.arrowBack);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(StateInfo.this, Informations.class );
+                startActivity(intent);
+            }
+        });
     }
 }
