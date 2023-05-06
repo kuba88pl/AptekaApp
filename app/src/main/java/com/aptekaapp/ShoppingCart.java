1470bba6a5945e23fe0a6acab0e3468d96187d17
Goldmabachmajorian <sebastian13333@gmail.com>
2023-05-06 23:38:24 +0200

    next steps

--- app/src/main/java/com/aptekaapp/ShoppingCart.java
+++ app/src/main/java/com/aptekaapp/ShoppingCart.java
@@ -2,13 +2,28 @@ package com.aptekaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ShoppingCart extends AppCompatActivity {
+
+    ImageButton backBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
+        backBtn=findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), MainScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
